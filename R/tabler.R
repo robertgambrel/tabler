10ffa3a79cf7e980d66e6220f085df6523be6222
robertgambrel <robert.gambrel@gmail.com>
2016-09-13 15:26:44 -0500

    Fix package description

---
+++ R/tabler.R
@@ -0,0 +1,15 @@
+#' @title tabler: a wrapper to quickly combine and share tidy regression output
+#' @name tabler
+#'
+#' @description The tabler package is a tool used to make simple tables of
+#'   regression output from multiple models that at least partially overlap in
+#'   variables used. It reports coeffecients, test statistics for variables,
+#'   and, if desired, model fit stats. It can output data as either csv or xls
+#'   and is meant to allow analysts to easily and quickly arrange a series of
+#'   regression outputs to share with collaborators.
+#'
+#'
+#' @docType package
+#' @aliases tabler tabler-package
+
+NULL
