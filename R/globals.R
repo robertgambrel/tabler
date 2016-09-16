511a3aea48afc9fda1112d9bf65770ccc8e17c07
robertgambrel <robert.gambrel@gmail.com>
2016-09-16 08:26:54 -0500

    Update notes about which files affected

--- R/globals.R
+++ R/globals.R
@@ -1,5 +1,6 @@
 # Declare global variables to get around check()'s list of notes about NSE
-# variables from dplyr and tidyr not showing up in the namespace
+# variables from dplyr and tidyr not showing up in the namespace. Fixes notes
+# about undeclared variables in convert_to_data and tablify functions.
 
 if(getRversion() >= "2.15.1") {
   utils::globalVariables(c("p.value", "displayed_stars", "estimate", "term",
